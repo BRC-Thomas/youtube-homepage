58a97a235cc71ee03693d294865489365bd692e4
BRC-Thomas <thomasbaracani@gmail.com>
2023-10-16 09:57:38 +0200

    Video grid + format duration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import CategoryPills from "./components/CategoryPills";
 import PageHeader from "./layouts/PageHeader";
-import { categories } from "./data/home";
+import { categories, videos } from "./data/home";
 import { useState } from "react";
+import VideoGridItem from "./components/VideoGridItem";
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
@@ -17,6 +18,11 @@ function App() {
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map(video => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
